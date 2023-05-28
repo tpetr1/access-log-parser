cc2f9fff854a071779854790fba666bbf739cd1b
pvt1 <petr.tatarkin@yandex.ru>
2023-05-28 18:52:28 +0400

    add class SumLastNums.java

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,8 @@ public class Main {
     public static void main(String[] args) {
         //task 1
         System.out.println("Дробная часть числа: " + Fraction.fraction(4.898797897979899790));
-
+        //task 2
+        System.out.println("Сумма 2х последних знаков числа: " + SumLastNums.sumLastNums(105667));
     }
 
 
---
+++ src/SumLastNums.java
@@ -0,0 +1,9 @@
+public class SumLastNums {
+    public static int sumLastNums(int x){
+
+        int m = x % 10;
+        x = x / 10;
+        int t = x % 10;
+        return m+t;
+    }
+}
