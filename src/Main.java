e273379c348d4f1fec75200c701db736458b6c8a
pvt1 <petr.tatarkin@yandex.ru>
2023-05-28 18:06:59 +0400

    add operation with numbers

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Случайное число от 0 до 1: " + Math.random());
+        System.out.println("Введите первое число: ");
+        int number1 = new Scanner(System.in).nextInt();
+        System.out.println("Введите второе число: ");
+        int number2 = new Scanner(System.in).nextInt();
+        //сумма
+        int sum = number1 + number2;
+        System.out.println("Сумма чисел: " + sum);
+        //разность
+        int diff = number1 - number2;
+        System.out.println("Разность чисел: " + diff);
+        //произведение
+        int multi = number1 * number2;
+        System.out.println("Произведение чисел: " + multi);
+        //частное
+        double quo = (double) number1 / number2;
+        System.out.println("Частное чисел: " + quo);
     }
 }
