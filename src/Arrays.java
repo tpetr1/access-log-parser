89c70d58f0704260e6f9b7a68258f4c8a34c9880
pvt1 <petr.tatarkin@yandex.ru>
2023-06-05 14:21:20 +0400

    fixed the condition for method leftTriangle

--- src/Arrays.java
+++ src/Arrays.java
@@ -55,7 +55,7 @@ public class Arrays {
     }
 
     public static void leftTriangle(int x){
-        for (int i = x; i > 0; i--){
+        for (int i = 0; i < x; i++){
             for (int j = 0; j < i; j++){
                 System.out.print('*');
             }
