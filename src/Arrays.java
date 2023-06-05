93b2045757f5671a7d52c71c49f14c16130a43ba
pvt1 <petr.tatarkin@yandex.ru>
2023-06-05 20:40:26 +0400

    fixed the condition for method countPositive

--- src/Arrays.java
+++ src/Arrays.java
@@ -131,7 +131,7 @@ public class Arrays {
     public static int countPositive(int[] arr){
         int count = 0;
         for (int i = 1; i < arr.length; i++){
-            if (arr[i] >= 0) {
+            if (arr[i] > 0) {
                count++;
             }
         }
