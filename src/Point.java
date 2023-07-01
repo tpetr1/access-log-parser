848daae7744ed89d504f9b6b93f4d44bfd90fac6
pvt1 <petr.tatarkin@yandex.ru>
2023-07-01 19:27:42 +0400

    sdd subclass Point with 3 points

--- src/Point.java
+++ src/Point.java
@@ -1,18 +1,22 @@
 public class Point {
     int x;
     int y;
+    int z;
 
     public Point(int x, int y) {
         this.x = x;
         this.y = y;
     }
 
+    public Point(int x, int y, int z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
     @Override
     public String toString() {
-        return "Point{" +
-                "x=" + x +
-                ", y=" + y +
-                '}';
+        return "Point{" + x + ", " + y;
     }
     public boolean Equal(Point a, Point b){
         return a.equals(b);
