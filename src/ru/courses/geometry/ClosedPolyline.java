454d0d347f4da32968541f2f651b58287eb7dbef
pvt1 <petr.tatarkin@yandex.ru>
2023-08-05 17:08:48 +0400

    fix. change method lengthClose for ClosedPolyline

--- src/ru/courses/geometry/ClosedPolyline.java
+++ src/ru/courses/geometry/ClosedPolyline.java
@@ -1,19 +1,13 @@
 package ru.courses.geometry;
 
 public class ClosedPolyline extends Polyline {
-    @Override
-    public double length() {
-        double sum = 0;
-        for (int i = 0; i < this.arrayPoints.size() - 1; i++) {
-            int a = Math.abs(this.arrayPoints.get(i).x - this.arrayPoints.get(i + 1).x);
-            int b = Math.abs(this.arrayPoints.get(i).y - this.arrayPoints.get(i + 1).y);
-            sum += Math.sqrt((a * a) + (b * b));
-            if (i == this.arrayPoints.size() - 2){
-                a = Math.abs(this.arrayPoints.get(0).x - this.arrayPoints.get(i + 1).x);
-                b = Math.abs(this.arrayPoints.get(0).y - this.arrayPoints.get(i + 1).y);
-                sum += Math.sqrt((a * a) + (b * b));
-            }
-        }
+
+    public double lengthClose() {
+        double sum = length();
+        int i = this.arrayPoints.size() - 2;
+        int a = Math.abs(this.arrayPoints.get(0).x - this.arrayPoints.get(i + 1).x);
+        int b = Math.abs(this.arrayPoints.get(0).y - this.arrayPoints.get(i + 1).y);
+        sum += Math.sqrt((a * a) + (b * b));
         return sum;
     }
 }
